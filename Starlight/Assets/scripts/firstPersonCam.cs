a69ec388030055c9c659f3110b53c12161ce69eb
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-22 00:36:34 +0900

    feat: 캐릭터 수직 이동 및 1인칭 카메라 구현

---
+++ Starlight/Assets/scripts/firstPersonCam.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstPersonCam : MonoBehaviour
+{
+    public float turnSpeed = 4.0f; // 마우스 회전 속도    
+    private float xRotate = 0.0f; // 내부 사용할 X축 회전량은 별도 정의 ( 카메라 위 아래 방향 )
+    public float moveSpeed = 5f; // 이동 속도 설정
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 좌우로 움직인 마우스의 이동량 * 속도에 따라 카메라가 좌우로 회전할 양 계산
+        float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
+        // 현재 y축 회전값에 더한 새로운 회전각도 계산
+        float yRotate = transform.eulerAngles.y + yRotateSize;
+
+        // 위아래로 움직인 마우스의 이동량 * 속도에 따라 카메라가 회전할 양 계산(하늘, 바닥을 바라보는 동작)
+        float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
+        // 위아래 회전량을 더해주지만 -45도 ~ 80도로 제한 (-45:하늘방향, 80:바닥방향)
+        // Clamp 는 값의 범위를 제한하는 함수
+        xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
+
+        // 카메라 회전량을 카메라에 반영(X, Y축만 회전)
+        transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+
+        // 카메라의 forward 방향을 이용하여 이동 방향을 계산합니다.
+        Vector3 moveDirection = Camera.main.transform.forward;
+
+        // 이동 방향 벡터를 정규화하여 거리에 이동 속도를 곱한 값을 이동 벡터로 설정합니다.
+        Vector3 movement = moveDirection.normalized * moveSpeed * Time.deltaTime;
+
+        // 오브젝트의 위치를 이동 벡터만큼 이동시킵니다.
+        transform.position += movement;
+    }
+
+}
--- Starlight/Assets/scripts/player.cs
+++ Starlight/Assets/scripts/player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public float speed = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if()
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
