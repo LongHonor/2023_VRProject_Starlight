57ebb0573c19a2c7db12fc5b4a90dc5ea8735144
LongHonor <kyh011039@gmail.com>
2023-05-22 22:32:45 +0900

    1인칭 시점 이동 구현

--- Starlight/Assets/scripts/player.cs
+++ Starlight/Assets/scripts/player.cs
@@ -1,35 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public float speed = 2.0f;
+    public float speed = 10.0f;
+    public float gravity = -20f;
+
+    public float yVelocity = 0;
+
+    public Transform cameraTransform;
+    public CharacterController characterController;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
+        playerMove();
+    }
+
+    private void playerMove()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = new Vector3(_moveDirX, 0, _moveDirZ);
+
+        moveDirection = cameraTransform.TransformDirection(moveDirection);
+
+        moveDirection *= speed;
+
+        yVelocity += (gravity * Time.deltaTime);
+
+        moveDirection.y = yVelocity;
+
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
