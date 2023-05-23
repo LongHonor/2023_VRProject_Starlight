24b301543647bd500a361151695e7e1fa1daee2b
LongHonor <kyh011039@gmail.com>
2023-05-23 21:30:57 +0900

    드래그 이동 구현

--- Starlight/Assets/scripts/DragMovement.cs
+++ Starlight/Assets/scripts/DragMovement.cs
@@ -5,30 +5,25 @@ using UnityEngine;
 public class DragMovement : MonoBehaviour
 {
     private Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnMouseDrag()
-    {
-        // 마우스 위치를 게임 월드로 변환
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // 드래그 대상 오브젝트를 마우스 위치로 이동
-        transform.position = mousePos + offset;
-    }
+    private float zPos;
 
     private void OnMouseDown()
     {
-        // 마우스 클릭 시 거리 오프셋 계산
-        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        zPos = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        offset = gameObject.transform.position - GetMouseWorldPos();
     }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = zPos;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + offset;
+    }
+
+    
 }
