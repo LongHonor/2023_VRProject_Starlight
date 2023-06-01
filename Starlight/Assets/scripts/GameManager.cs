86b08885eb425be49b355fc9c7815c8d2744c636
LongHonor <kyh011039@gmail.com>
2023-06-01 20:22:50 +0900

    게임 매니저 동작 확인

--- Starlight/Assets/scripts/GameManager.cs
+++ Starlight/Assets/scripts/GameManager.cs
@@ -2,37 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    private static GameManger instance == null;
-
-    public MiniatureManager miniature;
     public bool isGameOver;
-
-    private void awake()
-    {
-        if(instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad();
-        }
-        else
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    public void onClickStartButton()
-    {
-        isGameOver = false;
+        isGameOver = false;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isGameOver) ;
+        if (!isGameOver) Debug.Log(isGameOver);
     }
 }
