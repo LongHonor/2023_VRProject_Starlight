1b0d2ac5057c58e2289fba73bb7dd61f16710b74
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-06-07 18:07:56 +0900

    ui: 메인 ui 활성화시 XRDeviceSimulator 움직임 속도 제거

--- Starlight/Assets/scripts/UI/MainEventHandlers.cs
+++ Starlight/Assets/scripts/UI/MainEventHandlers.cs
@@ -1,20 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
 
 public class MainEventHandlers : MonoBehaviour
 {
     public GameObject uiManagerObject;
+    public XRDeviceSimulator simulator;
     private UIActiveManager uiManager;
+    private float[] originalSimulatgorTranslateSpeed = { 0, 0, 0 };
 
     private void Start()
     {
         uiManager = uiManagerObject.GetComponent<UIActiveManager>();
+        originalSimulatgorTranslateSpeed[0] = simulator.keyboardXTranslateSpeed;
+        originalSimulatgorTranslateSpeed[1] = simulator.keyboardYTranslateSpeed;
+        originalSimulatgorTranslateSpeed[2] = simulator.keyboardZTranslateSpeed;
+        simulator.keyboardXTranslateSpeed = 0;
+        simulator.keyboardYTranslateSpeed = 0;
+        simulator.keyboardZTranslateSpeed = 0;
     }
 
     public void StartButtonClickHandler()
     {
         uiManager.InactiveAllUiCanvas();
+        simulator.keyboardXTranslateSpeed = originalSimulatgorTranslateSpeed[0];
+        simulator.keyboardYTranslateSpeed = originalSimulatgorTranslateSpeed[1];
+        simulator.keyboardZTranslateSpeed = originalSimulatgorTranslateSpeed[2];
     }
 
     public void DescriptionButtonClickHandler()
